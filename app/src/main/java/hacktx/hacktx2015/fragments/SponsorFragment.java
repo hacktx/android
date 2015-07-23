7e7e85af8d0c6f9b934895c7ef68a64d3cac7087
DrewRomanyk <drewiswaycool@gmail.com>
2015-07-23 00:03:34 -0500

    Changed the main activities into fragments, schedule page needs fixin

---
+++ app/src/main/java/hacktx/hacktx2015/fragments/SponsorFragment.java
@@ -0,0 +1,23 @@
+package hacktx.hacktx2015.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hacktx.hacktx2015.R;
+
+/**
+ * Created by Drew on 7/22/2015.
+ */
+public class SponsorFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_sponsors, container, false);
+
+        return root;
+    }
+}
