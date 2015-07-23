2c407ea7cbf1ed0fc5240dc323e5148812b22c99
Thomas Gaubert <tpgaubert@gmail.com>
2015-07-22 21:35:30 -0700

    Migrate schedule to Retrofit
    Still includes local caching, using gson

--- app/src/main/java/hacktx/hacktx2015/fragments/ScheduleDayFragment.java
+++ app/src/main/java/hacktx/hacktx2015/fragments/ScheduleDayFragment.java
@@ -1,6 +1,5 @@
 package hacktx.hacktx2015.fragments;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -11,14 +10,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -26,22 +23,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Locale;
 
 import hacktx.hacktx2015.R;
-import hacktx.hacktx2015.enums.EventType;
 import hacktx.hacktx2015.models.ScheduleCluster;
-import hacktx.hacktx2015.models.ScheduleEvent;
-import hacktx.hacktx2015.models.ScheduleSpeaker;
 import hacktx.hacktx2015.views.adapters.ScheduleClusterRecyclerView;
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Path;
 
 /**
  * Created by Drew on 6/28/15.
@@ -79,7 +68,7 @@ public class ScheduleDayFragment extends Fragment {
         swipeRefreshLayout.post(new Runnable() {
             @Override
             public void run() {
-                if(scheduleList.size() == 0) {
+                if (scheduleList.size() == 0) {
                     swipeRefreshLayout.setRefreshing(true);
                 }
             }
@@ -98,7 +87,7 @@ public class ScheduleDayFragment extends Fragment {
 
     class ScheduleDataAsyncTask extends AsyncTask<String, String, Void> {
 
-        private JSONArray jsonArray = null;
+        private ArrayList<ScheduleCluster> scheduleClusters;
         private boolean overrideCache;
 
         public ScheduleDataAsyncTask(boolean overrideCache) {
@@ -110,108 +99,34 @@ public class ScheduleDayFragment extends Fragment {
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
             long lastUpdated = preferences.getLong("scheduleLastUpdated", 0);
             if(System.currentTimeMillis() - lastUpdated < 3600000 && !overrideCache) {
-                jsonArray = getJsonFromFile();
+                scheduleClusters = getDataFromFile();
             } else {
-                jsonArray = getJsonFromUrl();
+                scheduleClusters = getDataFromUrl();
             }
 
             return null;
         }
 
         protected void onPostExecute(Void v) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
-            try {
-                for (int x = 0; x < jsonArray.length(); x++) {
-                    JSONObject schedClusterJson = jsonArray.getJSONObject(x);
-                    JSONArray eventsArrayJson = schedClusterJson.getJSONArray("eventsList");
-                    ArrayList<ScheduleEvent> events = new ArrayList<>();
-
-                    for (int n = 0; n < eventsArrayJson.length(); n++) {
-                        JSONObject event = eventsArrayJson.getJSONObject(n);
-                        EventType type;
-                        switch (event.getString("type")) {
-                            case "food":
-                                type = EventType.FOOD;
-                                break;
-                            case "education":
-                                type = EventType.EDUCATION;
-                                break;
-                            case "talk":
-                                type = EventType.TALK;
-                                break;
-                            default:
-                                type = EventType.TALK;
-                                break;
-                        }
-
-                        ArrayList<ScheduleSpeaker> speakers = new ArrayList<>();
-                        JSONArray speakerArray = event.getJSONArray("speakerList");
-                        for (int k = 0; k < speakerArray.length(); k++) {
-                            JSONObject speaker = speakerArray.getJSONObject(k);
-                            speakers.add(new ScheduleSpeaker(speaker.getInt("id"), speaker.getString("name"),
-                                    speaker.getString("organization"), speaker.getString("description"),
-                                    speaker.getString("imageUrl")));
-                        }
-
-                        events.add(new ScheduleEvent(event.getInt("id"), type, event.getString("name"),
-                                formatter.parse(event.getString("startDate")),
-                                formatter.parse(event.getString("endDate")),
-                                event.getString("location"), event.getString("description"), speakers));
-                    }
-
-                    scheduleList.add(new ScheduleCluster(schedClusterJson.getInt("id"),
-                            schedClusterJson.getString("name"), events));
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            RecyclerView.Adapter scheduleAdapter = new ScheduleClusterRecyclerView(scheduleList);
+            RecyclerView.Adapter scheduleAdapter = new ScheduleClusterRecyclerView(scheduleClusters);
             recyclerView.setAdapter(scheduleAdapter);
 
             swipeRefreshLayout.setRefreshing(false);
         }
 
-        private JSONArray getJsonFromUrl() {
-            String url = "http://texasgamer.me/projects/hacktx/schedule.json";
-            JSONArray result = new JSONArray();
-            try {
-                URL u = new URL(url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) u.openConnection();
-                httpURLConnection.setRequestMethod("GET");
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
+        private ArrayList<ScheduleCluster> getDataFromUrl() {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint("http://demo9695027.mockable.io")
+                    .build();
 
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line + '\n');
-                }
-                saveCache(stringBuilder.toString());
-                result = new JSONArray(stringBuilder.toString());
-                httpURLConnection.disconnect();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            return result;
+            ScheduleService scheduleService = restAdapter.create(ScheduleService.class);
+            ArrayList<ScheduleCluster> scheduleClusters = scheduleService.getSchedule(1);
+            saveCache(new Gson().toJson(scheduleClusters));
+            return scheduleService.getSchedule(1);
         }
 
-        private JSONArray getJsonFromFile() {
-            try {
-                return new JSONArray(readCache());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            return getJsonFromUrl();
+        private ArrayList<ScheduleCluster> getDataFromFile() {
+            return new Gson().fromJson(readCache(), new TypeToken<ArrayList<ScheduleCluster>>(){}.getType());
         }
 
         private void saveCache(String data) {
@@ -258,5 +173,10 @@ public class ScheduleDayFragment extends Fragment {
 
             return ret;
         }
+    }
+
+    interface ScheduleService {
+        @GET("/api/schedules/{day}")
+        ArrayList<ScheduleCluster> getSchedule(@Path("day") int day);
     }
 }
