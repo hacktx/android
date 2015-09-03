f525ad57e4c1e936758df029e7ca6beabff36120
Thomas Gaubert <tpgaubert@gmail.com>
2015-09-02 20:21:44 -0500

    More UI work

--- app/src/main/java/hacktx/hacktx2015/activities/CheckInActivity.java
+++ app/src/main/java/hacktx/hacktx2015/activities/CheckInActivity.java
@@ -1,14 +1,20 @@
 package hacktx.hacktx2015.activities;
 
 import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import hacktx.hacktx2015.R;
 
@@ -27,6 +33,7 @@ public class CheckInActivity extends AppCompatActivity {
         }
 
         setupTaskActivityInfo();
+        setupEmailCard();
     }
 
     @Override
@@ -49,6 +56,29 @@ public class CheckInActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.hacktx_blue_dark));
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.hacktx_red));
         }
+    }
+
+    private void setupEmailCard() {
+        findViewById(R.id.emailCardOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = (EditText) findViewById(R.id.emailCardEditText);
+                String email = emailEditText.getText().toString();
+                if(!email.isEmpty() && email.contains("@")) {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(emailEditText.getWindowToken(), 0);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                    builder.setTitle("Verify Email");
+                    builder.setMessage("Is " + email + " correct?");
+                    builder.setPositiveButton("Yes", null);
+                    builder.setNegativeButton("No", null);
+                    builder.show();
+                } else {
+                    Snackbar.make(findViewById(android.R.id.content), "Invalid email address.", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
