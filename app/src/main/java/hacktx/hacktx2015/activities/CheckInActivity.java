d9c33d534ea63382303e3292a302ebb74fc75258
Thomas Gaubert <tpgaubert@gmail.com>
2015-09-02 21:31:45 -0500

    First implementation of QR check-in system

--- app/src/main/java/hacktx/hacktx2015/activities/CheckInActivity.java
+++ app/src/main/java/hacktx/hacktx2015/activities/CheckInActivity.java
@@ -1,20 +1,30 @@
 package hacktx.hacktx2015.activities;
 
 import android.app.ActivityManager;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import hacktx.hacktx2015.R;
 
@@ -63,20 +73,82 @@ public class CheckInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText emailEditText = (EditText) findViewById(R.id.emailCardEditText);
-                String email = emailEditText.getText().toString();
+                final String email = emailEditText.getText().toString();
                 if(!email.isEmpty() && email.contains("@")) {
                     InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(emailEditText.getWindowToken(), 0);
 
-                    AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CheckInActivity.this, R.style.AppCompatAlertDialogStyle);
                     builder.setTitle("Verify Email");
                     builder.setMessage("Is " + email + " correct?");
-                    builder.setPositiveButton("Yes", null);
+                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            findViewById(R.id.emailCard).setVisibility(View.GONE);
+                            findViewById(R.id.codeCard).setVisibility(View.VISIBLE);
+                            dialog.dismiss();
+
+                            new Thread(new Runnable() {
+                                public void run() {
+                                    QRCodeWriter writer = new QRCodeWriter();
+                                    try {
+                                        BitMatrix bitMatrix = writer.encode(email, BarcodeFormat.QR_CODE, 512, 512);
+                                        int width = bitMatrix.getWidth();
+                                        int height = bitMatrix.getHeight();
+                                        final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                                        for (int x = 0; x < width; x++) {
+                                            for (int y = 0; y < height; y++) {
+                                                bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : ContextCompat.getColor(CheckInActivity.this, R.color.cardview_light_background));
+                                            }
+                                        }
+
+                                        CheckInActivity.this.runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                ((ImageView) findViewById(R.id.codeCardCode)).setImageBitmap(bmp);
+                                                findViewById(R.id.codeCardProgressBar).setVisibility(View.GONE);
+                                                findViewById(R.id.codeCardCode).setVisibility(View.VISIBLE);
+                                                findViewById(R.id.codeCardButtons).setVisibility(View.VISIBLE);
+                                            }
+                                        });
+                                    } catch (WriterException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }).start();
+                        }
+                    });
                     builder.setNegativeButton("No", null);
                     builder.show();
                 } else {
                     Snackbar.make(findViewById(android.R.id.content), "Invalid email address.", Snackbar.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        findViewById(R.id.emailCardCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.codeCardReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.codeCard).setVisibility(View.GONE);
+                findViewById(R.id.codeCardCode).setVisibility(View.GONE);
+                findViewById(R.id.codeCardButtons).setVisibility(View.GONE);
+                findViewById(R.id.codeCardProgressBar).setVisibility(View.VISIBLE);
+                ((EditText) findViewById(R.id.emailCardEditText)).setText("");
+                findViewById(R.id.emailCard).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.codeCardDone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
