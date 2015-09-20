7d8c16bf44d30edf5b267f2c196a7d966fb58a85
Thomas Gaubert <tpgaubert@gmail.com>
2015-09-20 13:00:44 -0500

    Opening Parse notification now calls onPushOpen

--- app/src/main/java/hacktx/hacktx2015/receivers/ParseNotificationReceiver.java
+++ app/src/main/java/hacktx/hacktx2015/receivers/ParseNotificationReceiver.java
@@ -2,6 +2,7 @@ package hacktx.hacktx2015.receivers;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.ContextCompat;
@@ -35,6 +36,10 @@ public class ParseNotificationReceiver extends ParsePushBroadcastReceiver {
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent openIntent = new Intent("com.parse.push.intent.OPEN");
+        openIntent.putExtras(intent.getExtras());
+        openIntent.setPackage(context.getPackageName());
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, openIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         Notification notification = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_alert)
@@ -42,7 +47,8 @@ public class ParseNotificationReceiver extends ParsePushBroadcastReceiver {
                 .setContentText(msg).setAutoCancel(true)
                 .setColor(ContextCompat.getColor(context, R.color.accent))
                 .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL).build();
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setContentIntent(pendingIntent).build();
 
         notificationManager.notify(0, notification);
 
