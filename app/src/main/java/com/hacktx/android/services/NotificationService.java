34e1a5c5926e6f29ee7761f9ed59a68bd38520f6
Thomas Gaubert <tpgaubert@gmail.com>
2017-10-16 14:37:38 -0500

    formatting

--- app/src/main/java/com/hacktx/android/services/NotificationService.java
+++ app/src/main/java/com/hacktx/android/services/NotificationService.java
@@ -41,7 +41,7 @@ public class NotificationService extends FirebaseMessagingService {
     private ConfigManager mConfigManager;
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
         mConfigManager = new ConfigManager();
     }
